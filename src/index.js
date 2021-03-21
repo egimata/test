780a98a5d161279de92a3017ea1ada1f2d42f479
Egi Mata <egi.mata1@gmail.com>
2021-03-21 21:23:34 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import { render } from 'react-dom'
+import React, { Suspense } from 'react'
+import { Canvas } from 'react-three-fiber'
+import { useProgress, Html } from '@react-three/drei'
+import { HashRouter as Router, Switch, Route, NavLink, Redirect } from 'react-router-dom'
+
+import Scene1 from './Scene1'
+import Scene2 from './Scene2'
+import Scene3 from './Scene3'
+
+import "./base.css"
+
+function Loader() {
+  const { progress } = useProgress()
+  return (
+    <Html center>
+      <span style={{ color: '#FFFFFF' }}>{progress} % loaded</span>
+    </Html>
+  )
+}
+
+function App(props) {
+  const { scene = 1 } = props
+  return (
+    <Canvas concurrent shadowMap camera={{ position: [0, 0, 5], fov: 70 }}>
+      <color attach="background" args={['#000']} />
+      <Suspense fallback={<Loader />}>
+        {scene === 1 && <Scene1 />}
+        {scene === 2 && <Scene2 />}
+        {scene === 3 && <Scene3 />}
+      </Suspense>
+      <ambientLight intensity={0.4} />
+    </Canvas>
+  )
+}
+
+function Body() {
+  return (
+    <Router>
+      <main>
+        <div className="frame">
+          <div className="frame__title-wrap">
+            <h1 className="frame__title">Egi Mata - Web Developer</h1>
+            <p className="frame__tagline">Smash the Cubes!!</p>
+          </div>
+          <div className="frame__links">
+            <a href="https://myecv.me/">My Website</a>
+            {/* <a href="https://tympanus.net/codrops/?p=51167">Article</a>
+            <a href="https://github.com/emmelleppi/codrops-r3f-mirrors">GitHub</a> */}
+          </div>
+          <div className="frame__demos">
+            <NavLink to="/" activeClassName="frame__demo--current" className="frame__demo">
+              EGI MATA
+            </NavLink>
+          </div>
+        </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <App scene={3} />
+            </Route>
+          </Switch>
+        </div>
+      </main>
+    </Router>
+  )
+}
+
+render(<Body />, document.querySelector('#root'))
